df4ae6e9d2a72615d5f94e4c069a95d4250dbedf
Latteholic <cafelatte@gmail.com>
2020-11-12 22:40:59 +0900

    ordersのルーティングとコントローラー記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'items#index'
   resources :users, only: %i[edit update]
   resources :items, only: %i[new create edit update show destroy]
+  resources :orders, only: %i[new create index]
 end
