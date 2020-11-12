528255b14517722ee5c36880c0627dddb9f0fb1f
Latteholic <cafelatte@gmail.com>
2020-11-13 00:45:33 +0900

    都道府県プルダウンメニュー実装

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,3 +1,14 @@
 class Shipment < ApplicationRecord
   belongs_to :order
+  belongs_to :prefecture
+
+  with_options presence: true do
+    validates:  zip_code
+    validates:  city
+    validates:  house_number
+    validates:  tel
+  end
+
+  validates: prefecture_id, numericality: { other_than: 1 }
+  validates: building_name
 end
