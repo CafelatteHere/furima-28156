7e9a8123a01c5364d1fb6b835cc3cb44ec68d248
Latteholic <cafelatte@gmail.com>
2020-11-12 23:26:19 +0900

    購入画面へのリンク実装
    またはordersのルーティング、コントローラー記述を訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,4 @@
 class OrdersController < ApplicationController
   def index
   end
-
-  def create
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :users, only: %i[edit update]
-  resources :items, only: %i[new create edit update show destroy]
-  resources :orders, only: %i[new create index]
+  resources :items, only: %i[new create edit update show destroy] do
+    resources :orders, only: %i[index]
+  end
 end
