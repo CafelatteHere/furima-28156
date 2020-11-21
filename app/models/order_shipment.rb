90378bfb20c83dbbe76ab999ce61b63580f8d6a8
Latteholic <cafelatte@gmail.com>
2020-11-21 12:53:21 +0900

    記述の訂正

--- app/models/order_shipment.rb
+++ app/models/order_shipment.rb
@@ -15,6 +15,6 @@ class OrderShipment
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id, token: token)
-    shipment = Shipment.create(prefecture_id: prefecture_id, zip_code: zip_code, city: city, house_number: house_number, building_name: building_name, tel: tel, order_id: order.id)
+    Shipment.create(prefecture_id: prefecture_id, zip_code: zip_code, city: city, house_number: house_number, building_name: building_name, tel: tel, order_id: order.id)
   end
 end
